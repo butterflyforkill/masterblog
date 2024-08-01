455f5faa8cfd70e523c37d6ba47b0ab7fd3d9b14
butterflyforkill <vasse.paul@gmail.com>
2024-08-01 15:12:13 +0200

    Add styles css for different pages, add addform

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import json_parcer
 
 app = Flask(__name__)
 
+blog_posts = json_parcer.load_data('data.json')
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    # add code here to fetch the job posts from a file
+    return render_template('index.html', posts=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        # We will fill this in the next step
+        pass
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
