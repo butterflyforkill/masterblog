c6a799c74d94c9c61f1e88343e09fdc50e25cdab
butterflyforkill <vasse.paul@gmail.com>
2024-08-02 16:40:17 +0200

    Add update method and the button

--- app.py
+++ app.py
@@ -20,6 +20,12 @@ def delete_post_by_id(post_id):
     return False  # Return False if post_id is not found
 
 
+def fetch_post_by_id(post_id):
+    for post in blog_posts:
+        if post['id'] == post_id:
+            return post
+
+
 @app.route('/')
 def index():
     return render_template('index.html', posts=blog_posts)
@@ -49,7 +55,30 @@ def delete(post_id):
 
     # If post with given id is not found, redirect to the home page
     return redirect(url_for('inxex'))
+
+
+@app.route('/update/<string:post_id>', methods=['GET', 'POST'])
+def update(post_id):
+    # Fetch the blog posts from the JSON file
+    post = fetch_post_by_id(post_id)
+    if post is None:
+        # Post not found
+        return "Post not found", 404
     
+    if request.method == 'POST':
+        # Update the post in the JSON file
+        # Redirect back to index
+        title = request.form.get('title')  # Get the title from the form
+        author = request.form.get('author')  # Get the author from the form
+        content = request.form.get('content')  # Get the content from the form
+        new_post = {'id': post_id, 'title': title, 'author': author, 'content': content}
+        blog_posts.append(new_post)  # Append the new post to the blog_posts list
+        json_parcer.write_file('data.json', blog_posts)  # Save the updated blog posts to the JSON file
+        return redirect(url_for('index')) 
+
+    # Else, it's a GET request
+    # So display the update.html page
+    return render_template('update.html', post=post)
 
 
 if __name__ == '__main__':
